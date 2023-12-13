a533f1b69e52591e52e627a002e35e9ef120cb12
Priyanka <deorapriyanka202@gmail.com>
2023-12-13 12:30:35 +0530

    modified contact page

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,6 +8,37 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import ContactImg from '../public/assets/contact.jpg';
 
 const Contact = () => {
+
+  const [query, setQuery] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    subject: ""
+  });
+  
+  // Update inputs value
+  const handleParam = () => (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuery((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+  // Form Submit function
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    Object.entries(query).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+    fetch("https://getform.io/f/9b40dbc3-bb63-475b-934e-4d4ec4dcbf76", {
+      method: "POST",
+      body: formData
+    }).then(() => setQuery({ name: "", phone: "", email: "", subject: "", message: "" }));
+  };
+
+
   return (
     <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
@@ -75,55 +106,77 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
-                method='POST'
-                encType='multipart/form-data'
+                onSubmit={formSubmit}
+                // action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                // method='POST'
+                // encType='multipart/form-data'
               >
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                   <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>Name</label>
+                    <label htmlFor='name' className='uppercase text-sm py-2'>Name</label>
                     <input
                       className='border-2 rounded-lg p-3 flex border-gray-300'
                       type='text'
                       name='name'
+                      required
+                      placeholder="Name"
+                      value={query.name}
+                      onChange={handleParam()}
                     />
                   </div>
                   <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>
+                    <label htmlFor='phone' className='uppercase text-sm py-2'>
                       Phone Number
                     </label>
                     <input
                       className='border-2 rounded-lg p-3 flex border-gray-300'
                       type='text'
                       name='phone'
+                      required
+                      placeholder="Phone"
+                      value={query.phone}
+                      onChange={handleParam()}
                     />
                   </div>
                 </div>
                 <div className='flex flex-col py-2'>
-                  <label className='uppercase text-sm py-2'>Email</label>
+                  <label htmlFor='email' className='uppercase text-sm py-2'>Email</label>
                   <input
                     className='border-2 rounded-lg p-3 flex border-gray-300'
                     type='email'
                     name='email'
+                    required
+                    placeholder="Email"
+                    value={query.email}
+                    onChange={handleParam()}
                   />
                 </div>
                 <div className='flex flex-col py-2'>
-                  <label className='uppercase text-sm py-2'>Subject</label>
+                  <label htmlFor='subject' className='uppercase text-sm py-2'>Subject</label>
                   <input
                     className='border-2 rounded-lg p-3 flex border-gray-300'
                     type='text'
                     name='subject'
+                    required
+                    placeholder="Subject"
+                    value={query.subject}
+                    onChange={handleParam()}
                   />
                 </div>
                 <div className='flex flex-col py-2'>
-                  <label className='uppercase text-sm py-2'>Message</label>
+                  <label htmlFor='message' className='uppercase text-sm py-2'>Message</label>
                   <textarea
                     className='border-2 rounded-lg p-3 border-gray-300'
                     rows='10'
+                    type="text"
                     name='message'
+                    required
+                    placeholder="Message"
+                    value={query.message}
+                    onChange={handleParam()}
                   ></textarea>
                 </div>
-                <button className='w-full p-4 text-gray-100 mt-4'>
+                <button type="submit" className='w-full p-4 text-gray-100 mt-4 bg-[#5651e5]'>
                   Send Message
                 </button>
               </form>
